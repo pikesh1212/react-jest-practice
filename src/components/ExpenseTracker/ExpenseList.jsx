eac5ac81b8862f16a59caa392ea298ce65d8c4e0
Pikesh Patel <pikesh.patel492@gmail.com>
2025-04-01 14:15:22 +0530

    test file for expense tracker

--- src/components/ExpenseTracker/ExpenseList.jsx
+++ src/components/ExpenseTracker/ExpenseList.jsx
@@ -6,56 +6,63 @@ function ExpenseList() {
 
   return (
     <div className="card p-4 shadow-sm">
-      <div style={{display:"flex", justifyContent:"space-between"}}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
         <h3 className="mb-3">Expense List</h3>
         <h3 className="mb-3">
-          Total: ₹ 
+          Total: ₹
           {expenses?.reduce((acc, expense) => acc + Number(expense.amount), 0)}
         </h3>
       </div>
       <hr />
       <div className="table-responsive d-none d-md-block">
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Category</th>
-            <th>Amount</th>
-            <th>Description</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {expenses.length>0 ?
-            expenses.map((expense) => (
-              <tr key={expense.id}>
-                <td>{expense.date}</td>
-                <td>{expense.category}</td>
-                <td>₹ {expense.amount}</td>
-                <td>{expense.description}</td>
-                <td>
-                  <button
-                    data-testid="edit-button"
-                    className="btn btn-warning btn-sm me-2"
-                    onClick={() => {
-                      handleEditExpense(expense);
-                      handleShow();
-                    }}
-                  >
-                    Edit
-                  </button>
-                  <button
-                    data-testid="delete-button"
-                    className="btn btn-danger btn-sm"
-                    onClick={() => removeExpense(expense.id)}
-                  >
-                    Delete
-                  </button>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Category</th>
+              <th>Amount</th>
+              <th>Description</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {expenses.length > 0 ? (
+              expenses.map((expense) => (
+                <tr key={expense.id}>
+                  <td>{expense.date}</td>
+                  <td>{expense.category}</td>
+                  <td>₹ {expense.amount}</td>
+                  <td>{expense.description}</td>
+                  <td>
+                    <button
+                      data-testid="edit-button"
+                      className="btn btn-warning btn-sm me-2"
+                      onClick={() => {
+                        handleEditExpense(expense);
+                        handleShow();
+                      }}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      data-testid="delete-button"
+                      className="btn btn-danger btn-sm"
+                      onClick={() => removeExpense(expense.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={5} className="text-center">
+                  No Expenses Found
                 </td>
               </tr>
-            )):<tr><td colSpan={5} className="text-center">No Expenses Found</td></tr>}
-        </tbody>
-      </table>
+            )}
+          </tbody>
+        </table>
       </div>
       <div className="d-md-none">
         {expenses.map((expense) => (
